236ba953b853e564d65627a8722cade6e1bbc03f
richy486 <richy486@gmail.com>
2015-07-15 14:36:07 +0200

    Initial Commit

---
+++ CloudinaryImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CloudinaryImage
+//
+//  Created by Richard Adem on 15/07/2015.
+//  Copyright (c) 2015 Richard Adem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CloudinaryImage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CloudinaryImage
+//
+//  Created by Richard Adem on 15/07/2015.
+//  Copyright (c) 2015 Richard Adem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
